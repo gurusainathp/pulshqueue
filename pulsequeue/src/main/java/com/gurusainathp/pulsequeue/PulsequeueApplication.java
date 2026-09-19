20a3afc494efffd601098002f2d5ce711882a772
gurusainathp <144239466+gurusainathp@users.noreply.github.com>
2026-09-19 13:48:57 +0530

    Spring Boot Setup

---
+++ pulsequeue/src/main/java/com/gurusainathp/pulsequeue/PulsequeueApplication.java
@@ -0,0 +1,13 @@
+package com.gurusainathp.pulsequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsequeueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsequeueApplication.class, args);
+	}
+
+}
---
+++ pulsequeue/src/test/java/com/gurusainathp/pulsequeue/PulsequeueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurusainathp.pulsequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsequeueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
